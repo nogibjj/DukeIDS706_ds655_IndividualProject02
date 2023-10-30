7ef87420a719baa42081394ee3034839f4e4c252
Divya Sharma <divya.sharma@duke.edu>
2023-10-30 04:20:19 +0000

    adding py codes

--- Codes/main.py
+++ Codes/main.py
@@ -1,6 +1,31 @@
-def main():
-    pass
+"""
+ETL-Query script
+"""
 
+from mylib.extract import extract
+from mylib.transform_load import load
+from mylib.query import query
+import argparse
 
-if __name__ == "__main__":
-    main()
+parser = argparse.ArgumentParser(description="Extract, Transform, or Load data")
+parser.add_argument(
+    "--step", type=int, help="Step number to run (1 - Extract, 2 - Load, or 3 - Query)"
+)
+args = parser.parse_args()
+# Extract
+if args.step == 1:
+    print("Extracting data...")
+    extract()
+
+# Transform and load
+elif args.step == 2:
+    print("Transforming data...")
+    load()
+
+# Query
+elif args.step == 3:
+    print("Querying data...")
+    query()
+
+else:
+    print("Invalid step number")
---
+++ Codes/test_main.py
@@ -0,0 +1,25 @@
+"""
+Code to test the ETL pipeline
+"""
+
+from mylib.extract import extract
+from mylib.transform_load import load
+from mylib.query import query
+
+
+def test_etl():
+    assert (query(load(extract()))) == "Success"
+    assert (
+        query(
+            load(
+                extract(
+                    "https://gist.githubusercontent.com/netj/8836201/raw/6f9306ad21398ea43cba4f7d537619d0e07d5ae3/iris.csv",
+                    "data/Iris_Data.csv",
+                )
+            )
+        )
+    ) == "Success"
+
+
+if __name__ == "__main__":
+    test_etl()
